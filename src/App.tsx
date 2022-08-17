e600aad6748e39e29ce3dceb4fa0d098cafff6f9
diakojamshidi <jack.jack.129946@gmail.com>
2022-08-17 09:41:14 +0430

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
           <Routes key={item.Key}>
             <Route path={item.Path} element={<item.Component />} />
           </Routes>
+          
         );
       })}
     </>
